cf4a8570ccab784d039ec6ba4407de0581dff06d
unknown <fcardoso@gamingcampus.local>
2024-03-07 12:31:51 +0100

    Correction bug

--- Assets/Scenes/Setup/Generator.cs
+++ Assets/Scenes/Setup/Generator.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
+    [SerializeField] private Transform startpos1;
+    [SerializeField] private Transform startpos2;
+    [SerializeField] private Transform startpos3;
+
 
     [SerializeField] private int obstacleCountTarget;
     [SerializeField] private float levelWidth;
@@ -76,6 +80,60 @@ public class Generator : MonoBehaviour
         return transform;
     }
 
+    public void GeneratePoisson()
+    {
+        obstacles = new List<Transform>();
+
+        List<Vector3> points = new List<Vector3>();
+        points.Add(Vector3.zero);
+
+        int count = 0, safe = 1000;
+        while (points.Count > 0 && count < obstacleCountTarget && safe > 0)
+        {
+            int selectedIndex = Random.Range(0, points.Count);
+            Vector3 point = points[selectedIndex];
+
+
+            int tries = triesCount;
+            while (tries > 0)
+            {
+                Vector3 newPos = point + GetRandomPoint();
+                bool valid = true;
+                for (int i = 0; i < obstacles.Count; i++)
+                {
+                    if (Vector3.Distance(newPos, new Vector3(obstacles[i].position.x, 0, obstacles[i].position.z)) < minDistance || !OnField(newPos) || TooClose(newPos))
+                    {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (valid)
+                {
+                    obstacles.Add(CreateObject(newPos));
+                    points.Add(newPos);
+                    count++;
+                    break;
+                }
+
+                tries--;
+            }
+
+            if (tries <= 0)
+            {
+                points.RemoveAt(selectedIndex);
+            }
+
+            safe--;
+        }
+    }
+
+    private Vector3 GetRandomPoint()
+    {
+        Vector2 randomPoint = Random.insideUnitCircle;
+        return new Vector3(randomPoint.x, 0, randomPoint.y) * spread;
+    }
+
     public void ClearObstacles()
     {
         foreach (Transform obstacle in obstacles)
@@ -83,4 +141,28 @@ public class Generator : MonoBehaviour
             GameObject.Destroy(obstacle.gameObject);
         }
     }
+
+    private bool OnField(Vector3 pos)
+    {
+        bool check = pos.x > -levelDepth * 0.5 && pos.x < levelDepth * 0.5 && pos.z > -levelWidth * 0.5 && pos.z < levelWidth * 0.5;
+        return check;
+    }
+
+    private bool TooClose(Vector3 pos)
+    {
+        List<Transform> startPoss = new List<Transform> { startpos1, startpos2, startpos3 };
+        bool valid = false;
+
+
+        foreach (Transform startPos in startPoss)
+        {
+            if (Vector3.Distance(pos, new Vector3(startPos.position.x, 0, startPos.position.z)) < 10)
+            {
+                valid = true; break;
+            }
+        }
+
+        return valid;
+    }
 }
+
